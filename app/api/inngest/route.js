c0ba8a4b0c796800e6da9079ec48c05ef3c52c51
Rakibulislam1 <rirakib03@gmail.com>
2025-10-13 14:57:09 +0600

    connect neon-db with prisma

---
+++ app/api/inngest/route.js
@@ -0,0 +1,19 @@
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import {
+  syncUserCreation,
+  syncUserDelete,
+  syncUserUpdating,
+} from "@/inngest/functions";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    /* your functions will be passed here later! */
+
+    syncUserCreation,
+    syncUserUpdating,
+    syncUserDelete,
+  ],
+});
