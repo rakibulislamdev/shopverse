77ddc3da1ae05f56a0723b34c5af2673c1059021
Rakibulislam1 <rirakib03@gmail.com>
2025-10-15 22:42:26 +0600

    seller store orders page functionality done

---
+++ app/api/store/orders/route.js
@@ -0,0 +1,63 @@
+import prisma from "@/lib/prisma";
+import authSeller from "@/middleware/authSeller";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// update seller order status
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const storeId = await authSeller(userId);
+
+    if (!storeId) {
+      return NextResponse.json({ error: "Not Authorized" }, { status: 401 });
+    }
+
+    const { orderId, status } = await request.json();
+
+    await prisma.order.update({
+      where: { id: orderId, storeId },
+      data: { status },
+    });
+
+    return NextResponse.json(
+      { message: "Order status updated" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// get all orders for a seller
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+    const storeId = await authSeller(userId);
+
+    if (!storeId) {
+      return NextResponse.json({ error: "Not Authorized" }, { status: 401 });
+    }
+    const orders = await prisma.order.findMany({
+      where: { storeId },
+      include: {
+        user: true,
+        address: true,
+        orderItems: { include: { product: true } },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json({ orders }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
