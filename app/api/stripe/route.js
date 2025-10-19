f4c1901f656396d605d43053c3193068d9f08ac3
Rakibulislam1 <rirakib03@gmail.com>
2025-10-19 22:59:11 +0600

    stripe added done

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -6,8 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
   try {
-    const body = await request.json();
-    const sig = request.get("stripe-signature");
+    const body = await request.text();
+    const sig = request.headers.get("stripe-signature");
     const event = stripe.webhooks.constructEvent(
       body,
       sig,
